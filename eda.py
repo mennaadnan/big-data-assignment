7c97d2df46a0a03db86d576c4cdfdc5b14d3c7b6
mennaadnan <122840643+mennaadnan@users.noreply.github.com>
2025-01-08 10:24:01 +0200

    eda.py

--- eda.py
+++ eda.py
@@ -3,22 +3,19 @@ def eda(df):
     print(df.describe().T)
 
     # Insight 1: Null values
-    null_count = 0
-    for count in df.isnull().sum():
-        null_count += count  # Summing the null values
-    with open("eda-in-1.txt", "a") as eda1:
+    null_count = df.isnull().sum().sum()  # Total null values
+    with open("eda-in-1.txt", "w") as eda1:
         eda1.write(f"There are {null_count} rows with null content.\n")
 
 
     # Insight 2: Duplicates
     duplicated_count = df.duplicated().sum()
-    with open("eda-in-2.txt", "a") as eda2:
+    with open("eda-in-2.txt", "w") as eda2:
         eda2.write(f"There are {duplicated_count} duplicated row(s) in the data.\n")
 
     # Insight 3: Unique values in 'Customer Status'
-    unique_status = df['Customer Status'].unique()
-    uniq_values = ', '.join(map(str, unique_status))  # Show before encoding
-    with open("eda-in-3.txt", "a") as eda3:
+    uniq_values = ','.join(df['Customer Status'].unique())
+    with open("eda-in-3.txt", "w") as eda3:
         eda3.write(f"The column Customer Status has {len(df['Customer Status'].unique())} unique values: {uniq_values}\n")
     
     # Proceed to next stage (visualization)
